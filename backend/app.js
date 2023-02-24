2c71beda99fcc216891df5450395cddc339309a7
yhadari <yhadari@student.1337.ma>
2023-02-24 01:14:30 +0100

    setup docker-compose and prisma

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,43 @@ const cors = require("cors");
 const morgan = require("morgan");
 
 const app = express();
-const port = 3000;
+const port = 8000;
+
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+
+const main = async () => {
+  await prisma.user.create({
+    data: {
+      name: "yassine",
+      email: "hadariyassine1@gmail.com",
+      posts: {
+        create: { title: "Hello World" },
+      },
+      profile: {
+        create: { bio: "I like turtles" },
+      },
+    },
+  });
+
+  const allUsers = await prisma.user.findMany({
+    include: {
+      posts: true,
+      profile: true,
+    },
+  });
+  console.dir(allUsers, { depth: null });
+};
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
 
 //-----------------MIDDLEWARES-----------------
 app.use(morgan("dev"));
